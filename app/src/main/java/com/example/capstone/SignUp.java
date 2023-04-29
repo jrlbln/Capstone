de1e3baec66c385dc5b18507490b27052ec09108
jrlbln <joerelbelen02@gmail.com>
2023-04-29 17:26:08 +0100

    Added SignUp Layout

---
+++ app/src/main/java/com/example/capstone/SignUp.java
@@ -0,0 +1,14 @@
+package com.example.capstone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up);
+    }
+}
