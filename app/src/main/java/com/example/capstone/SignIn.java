1be4b84fbdc4bf2c1424b0e79ac72f9240a49b26
jrlbln <joerelbelen02@gmail.com>
2023-05-02 14:19:29 +0100

    added Home page

--- app/src/main/java/com/example/capstone/SignIn.java
+++ app/src/main/java/com/example/capstone/SignIn.java
@@ -19,7 +19,7 @@ public class SignIn extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(SignIn.this, LandingPage.class);
+                Intent intent = new Intent(SignIn.this, Home.class);
                 startActivity(intent);
             }
         });
