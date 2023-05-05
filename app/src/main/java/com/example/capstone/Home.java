81d59a0368d6335d6b9c20a53d9bf7f45ba736eb
jrlbln <joerelbelen02@gmail.com>
2023-05-05 11:48:40 +0100

    added limited-row Table

--- app/src/main/java/com/example/capstone/Home.java
+++ app/src/main/java/com/example/capstone/Home.java
@@ -3,25 +3,38 @@ package com.example.capstone;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 import androidx.core.view.GravityCompat;
 import androidx.core.view.MenuItemCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 import com.google.android.material.navigation.NavigationView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Home extends AppCompatActivity {
 
     private DrawerLayout drawerLayout;
     private NavigationView navigationView;
+    private TableLayout tableLayout;
+    private String[] headers = {"Name", "Age", "Gender"};
+    private List<String[]> data = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +43,21 @@ public class Home extends AppCompatActivity {
 
         drawerLayout = findViewById(R.id.drawer_layout);
         navigationView = findViewById(R.id.nav_view);
+        tableLayout = findViewById(R.id.table_layout);
+
+
+
+        // Add some sample data
+        data.add(new String[]{"John Doe", "25", "Male"});
+        data.add(new String[]{"Jane Smith", "30", "Female"});
+        data.add(new String[]{"Bob Johnson", "40", "Male"});
+        data.add(new String[]{"Alice Brown", "22", "Female"});
+        data.add(new String[]{"Tom Jones", "50", "Male"});
+        data.add(new String[]{"Sue Davis", "28", "Female"});
+        data.add(new String[]{"Sam Wilson", "35", "Male"});
+        data.add(new String[]{"Eva Lee", "29", "Female"});
+        data.add(new String[]{"Mike Smith", "45", "Male"});
+        data.add(new String[]{"Lisa Chen", "27", "Female"});
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawerLayout, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -83,9 +111,35 @@ public class Home extends AppCompatActivity {
 
         //add new functions here
 
+        // Add the table header
+        TableRow headerRow = new TableRow(this);
+        for (String header : headers) {
+            TextView textView = new TextView(this);
+            textView.setText(header);
+            textView.setTextSize(20);
+            textView.setPadding(10, 10, 10, 10);
+            textView.setBackgroundColor(getResources().getColor(R.color.white));
+            textView.setTextColor(getResources().getColor(R.color.purple));
+            headerRow.addView(textView);
+        }
+        tableLayout.addView(headerRow);
 
-
-
+        // Add the table data
+        int numRowsToShow = 5;
+        for (int i = 0; i < Math.min(data.size(), numRowsToShow); i++) {
+            TableRow dataRow = new TableRow(this);
+            String[] row = data.get(i);
+            for (String rowData : row) {
+                TextView textView = new TextView(this);
+                textView.setText(rowData);
+                textView.setTextSize(16);
+                textView.setBackgroundColor(getResources().getColor(R.color.white));
+                textView.setTextColor(getResources().getColor(R.color.light_purple));
+                textView.setPadding(10, 10, 10, 10);
+                dataRow.addView(textView);
+            }
+            tableLayout.addView(dataRow);
+        }
     }
     //exit drawer
     @Override
